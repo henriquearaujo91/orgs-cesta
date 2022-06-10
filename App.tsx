3e9ac40ddf07729a9d6d50c138c6863597537ff4
Henrique Araujo <henrique_araujo91@outlook.com>
2022-06-10 17:47:34 -0300

    Fonte externa
    Importando font do google no projeto.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,19 @@
-import { SafeAreaView, StatusBar } from 'react-native';
+import { SafeAreaView, StatusBar, View } from 'react-native';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+
+
 import Cesta from './src/telas/Cesta';
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold
+  });
+
+  if (!fonteCarregada) {
+    return <View />
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
