ec3992be39e8a0ab54a78932c884daa0fb84f96e
Henrique Araujo <henrique_araujo91@outlook.com>
2022-06-10 12:45:15 -0300

    Imagem e estilos
    Utilizando Dimensions e configurando StyleSheet para formatar a imagem na tela.

--- src/telas/Cesta.tsx
+++ src/telas/Cesta.tsx
@@ -1,5 +1,31 @@
-import { Text } from "react-native";
+import { Image, StyleSheet, Dimensions, Text } from "react-native";
+
+import topo from "../../assets/topo.png";
+
+const widthScreen = Dimensions.get("screen").width;
+const heightImage = 578;
+const widthImage = 768;
 
 export default function Cesta() {
-    return <Text>Cesta</Text>
+    return <>
+        <Image source={topo} style={estilos.topo} />
+        <Text style={estilos.titulo}>Detalhe da cesta</Text>
+    </>
 }
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: heightImage / widthImage * widthScreen,
+    },
+    titulo: {
+        width: "100%",
+        position: "absolute",
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight: 26,
+        color: "#fff",
+        fontWeight: "bold",
+        padding: 16
+    }
+})
